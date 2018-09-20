718e27d7133f32f7c628cc3fbea1ccbdbbd555f5
Ishmael Sunday <9257966+okayishmael@users.noreply.github.com>
2018-09-19 23:04:56 -0400

    Update busmall.js

--- busmall.js
+++ busmall.js
@@ -86,7 +86,7 @@ function imgClicked(event) {
    //check..
    if(event.target.id === firstImg.id) {
        //do this
-     firstImg.clicked += 1;
+     firstImg.clicked += 0;
    }else if(event.target.id === secondImg.id) {
        //do this
    secondImg.clicked += 1;
